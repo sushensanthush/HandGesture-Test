43042881fc6024663f5dcd14f6dae22cfd67afe8
Susa <sushensanthush02@gmail.com>
2026-06-13 21:57:41 +0530

    Add hand landmark detection functionality
    - Introduced a new task file for hand landmark detection.
    - Implemented hand landmark detection in index.py using MediaPipe.
    - Configured the hand landmarker with a specified model and processed an input image.

---
+++ index.py
@@ -0,0 +1,14 @@
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+baseOption = python.BaseOptions(model_asset_path="hand_landmarker.task")
+options = vision.HandLandmarkerOptions(base_options=baseOption, num_hands=1)
+detector = vision.HandLandmarker.create_from_options(options)
+
+image = mp.Image.create_from_file("hand.jpg")
+result = detector.detect(image)
+
+annotatedImage = draw_landmarks_on_image(image.numpy_view(), result)
+cv2.imshow(cv2.cvtColor(annotatedImage, cv2.COLOR_RGB2BGR))
