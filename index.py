12d529fc0ae8d1e85d4be41dec9c4d266d5bdacc
Susa <sushensanthush02@gmail.com>
2026-06-13 22:07:09 +0530

    Add hand landmark detection and visualization in main function

--- index.py
+++ index.py
@@ -44,3 +44,14 @@ def draw_landmarks_on_image(rgb_image, detection_result):
                 FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
 
   return annotated_image
+
+baseOption = python.BaseOptions(model_asset_path="hand_landmarker.task")
+options = vision.HandLandmarkerOptions(base_options=baseOption, num_hands=1)
+detector = vision.HandLandmarker.create_from_options(options)
+
+image = mp.Image.create_from_file("hand.jpg")
+result = detector.detect(image)
+
+annotatedImage = draw_landmarks_on_image(image.numpy_view(), result)
+cv2.imshow('Image',)
+cv2.waitKey(0)
