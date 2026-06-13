333848ba62b15df2eff3430bec3ff1dd6e45c8e4
Susa <sushensanthush02@gmail.com>
2026-06-13 22:02:46 +0530

    Fix import order by moving numpy import to the correct position

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+import numpy as np
 
 baseOption = python.BaseOptions(model_asset_path="hand_landmarker.task")
 options = vision.HandLandmarkerOptions(base_options=baseOption, num_hands=1)
